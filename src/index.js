0496319a12dc75291d147ed121e1a76a4eac9cb0
Evan Wu <evanwu.twn@gamil.com>
2018-03-28 19:09:33 +0800

    新增 mobile menu

--- src/index.js
+++ src/index.js
@@ -12,7 +12,20 @@ function headScroll() {
   });
 }
 
+// nav-collapse
+function navCollapse() {
+  var burgerIcon = $('.burg');
+  var container = $('.header');
+  var navBtn = $('.nav-ctrl-btn');
+
+  navBtn.on('click', function () {
+    burgerIcon.toggleClass('activeBurg');
+    container.toggleClass('open-menu');
+  });
+}
+
 // docmuent ready
 $(document).ready(() => {
   headScroll();
+  navCollapse();
 });
